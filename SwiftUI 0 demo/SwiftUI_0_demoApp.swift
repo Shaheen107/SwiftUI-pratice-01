eff13c1af21045fa36e07012742239f6b1ba3551
Shaheen107 <mrizwantariq107@gmail.com>
2024-07-18 22:38:10 +0500

    Initial Commit

---
+++ SwiftUI 0 demo/SwiftUI_0_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_0_demoApp.swift
+//  SwiftUI 0 demo
+//
+//  Created by Apple on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_0_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
